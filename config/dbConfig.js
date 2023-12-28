79cd8f46705abbb21c2799f1d792fb2b3847bec3
F.LIE <mouseanone28@gmail.com>
2023-12-28 13:32:07 +0700

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,6 @@
 const MongoDBConfig = {
   // test
   url: "mongodb://localhost:27017/test",
-  // prod
-  // url: "mongodb+srv://general-pos:ANIMATRONIC@general-pos.lawkplr.mongodb.net/",
   connectOption: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
