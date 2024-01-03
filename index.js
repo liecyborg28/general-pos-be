1d2ef6e34e1efe3bd4c5de66fc65aea0302882ac
F.LIE <mouseanone28@gmail.com>
2024-01-03 12:05:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,18 +14,17 @@ mongoose
   .then(() => console.log("MongoDB connected!"))
   .catch((err) => console.log(err));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
-
-// Middleware ini akan mengatur header CORS untuk semua rute
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // Mengizinkan akses dari berbagai domain
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
 
 app.use(routes);
 
