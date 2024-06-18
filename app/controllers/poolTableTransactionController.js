e5b2fb950a93e13d5fe9b4764d1eefe6fbd75569
F.LIE <mouseanone28@gmail.com>
2024-06-19 00:11:29 +0700

    Update poolTableTransactionController.js

--- app/controllers/poolTableTransactionController.js
+++ app/controllers/poolTableTransactionController.js
@@ -1,9 +1,64 @@
 const User = require("../models/userModel");
-const Item = require("../models/itemModel");
 const Transaction = require("../models/transactionModel");
 const pageController = require("./utils/pageController");
 const poolTableController = require("./");
-const inventoryController = require("./inventoryController");
 const errorMessages = require("../repository/messages/errorMessages");
 const successMessages = require("../repository/messages/successMessages");
 const logController = require("./logController");
+
+function convertToLocaleISOString(date, type) {
+  if (type !== "start" && type !== "end") {
+    throw new Error('Parameter "type" harus "start" atau "end"');
+  }
+
+  // if (type === "start") date.setDate(date.getDate() + 1);
+
+  return date
+    .toISOString()
+    .replace(
+      /T\d{2}:\d{2}:\d{2}.\d{3}Z/,
+      type === "start" ? "T00:00:00.000Z" : "T23:59:59.999Z"
+    );
+}
+
+module.exports = {
+  createPoolTableTransaction: async (req) => {
+    let dateISOString = new Date().toISOString();
+    let body = req.body;
+
+    const bearerHeader = req.headers["authorization"];
+    const bearerToken = bearerHeader.split(" ")[1];
+
+    let userByToken = await User.findOne({
+      "auth.accessToken": bearerToken,
+    });
+  },
+
+  getPoolTableTransactions: (req) => {
+    let pageKey = req.query.pageKey ? req.query.pageKey : 1;
+    let pageSize = req.query.pageSize ? req.query.pageSize : null;
+  },
+
+  getPoolTableTransactionsByPeriod: (req) => {
+    let pageKey = req.query.pageKey ? req.query.pageKey : 1;
+    let pageSize = req.query.pageSize
+      ? req.query.pageSize
+      : 1 * 1000 * 1000 * 1000;
+
+    let defaultFrom = convertToLocaleISOString(new Date(), "start");
+    let defaultTo = convertToLocaleISOString(new Date(), "end");
+  },
+
+  updatePoolTableTransaction: async (req) => {
+    let dateISOString = new Date().toISOString();
+
+    const bearerHeader = req.headers["authorization"];
+    const bearerToken = bearerHeader.split(" ")[1];
+
+    let userByToken = await User.findOne({
+      "auth.accessToken": bearerToken,
+    });
+
+    let body = req.body;
+  },
+};
